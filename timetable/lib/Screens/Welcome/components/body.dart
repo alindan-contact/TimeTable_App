c9e0362e6a692cc3129c1d91b1a72f5855f20aed
Alin Dan <alindan@Alins-MBP.lan>
2022-11-05 16:55:41 +0200

    save 5.Nov

--- timetable/lib/Screens/Welcome/components/body.dart
+++ timetable/lib/Screens/Welcome/components/body.dart
@@ -5,10 +5,44 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     // this size provide us total of width and height of our screen
     Size size = MediaQuery.of(context).size;
+    return Background(size: size);
+  }
+}
+
+class Background extends StatelessWidget {
+  const Background({
+    Key? key,
+    required this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       height: size.height,
       width: double.infinity,
-      child: Stack(),
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Image.asset(
+              "assets/images/main_top.png",
+              width: size.width * 0.3,
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: Image.asset(
+              "assets/images/main_bottom.png",
+              width: size.width * 0.2,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
